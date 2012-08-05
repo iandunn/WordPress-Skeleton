fbfa8864b5572b81e2dcd343033da852c6ca9a99
Ian Dunn <ian_dunn@yahoo.com>
2012-08-04 18:36:01 -0700

    Anal-retentive whitespace fix

--- wp-config.php
+++ wp-config.php
@@ -7,7 +7,7 @@
 require_once( dirname( __FILE__ ) . '/database.php' );
 
 define( 'WP_CONTENT_DIR',	dirname( __FILE__ ) . '/content' );
-define( 'WP_CONTENT_URL',	'http://'. $_SERVER[ 'HTTP_HOST' ] .'/content' );
+define( 'WP_CONTENT_URL',	'http://'. $_SERVER[ 'HTTP_HOST' ] . '/content' );
 define( 'WPLANG',               '' );
 define( 'WP_DEBUG',             false );
 
