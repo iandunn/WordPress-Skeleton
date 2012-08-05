c637a65c957af545fd1eb55a769676ec2320bbb3
Ian Dunn <ian_dunn@yahoo.com>
2012-08-05 09:36:33 -0700

    database.php to instance-config.php, added ?>

--- wp-config.php
+++ wp-config.php
@@ -4,15 +4,15 @@
  * @package WordPress
  */
 
-require_once( dirname( __FILE__ ) . '/database.php' );
+require_once( dirname( __FILE__ ) . '/instance-config.php' );
 
-define( 'WP_CONTENT_DIR',	dirname( __FILE__ ) . '/content' );
+define( 'WP_CONTENT_DIR',        dirname( __FILE__ ) . '/content' );
 define( 'WP_CONTENT_URL',	'http://'. $_SERVER[ 'HTTP_HOST' ] . '/content' );
 define( 'WPLANG',               '' );
-define( 'WP_DEBUG',             false );
 
 if( !defined( 'ABSPATH' ) )
-        define( 'ABSPATH', dirname(__FILE__) . '/' );
+        define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 
 require_once( ABSPATH . 'wp-settings.php' );
 
+?>
