2faaa7fc67a13982e9ab7958dd1df36e910fbf95
Ian Dunn <ian_dunn@yahoo.com>
2012-08-07 11:14:21 -0700

    Registering extra theme directory

--- content/mu-plugins/wordpress-skeleton-functionality.php
+++ content/mu-plugins/wordpress-skeleton-functionality.php
@@ -1,4 +1,13 @@
 <?php
+/*
+Plugin Name: WordPress Skeleton Functionality
+Plugin URI: https://github.com/iandunn/WordPress-Skeleton
+Description: Random snippets for the WordPress Skeleton deployment
+Version: 0.1
+Author: Ian Dunn
+Author URI: http://iandunn.name
+License: GPL2
+*/
 
 if( !defined( 'ABSPATH' ) )
     die( 'WordPress Skeleton Functionality error' );
