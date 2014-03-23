f24d71cbc3481207ed4d7922cff6b9790d1b7efd
Ian Dunn <ian@iandunn.name>
2014-03-22 19:22:52 -0700

    Add comment to remind admins about log privacy

--- environment-config-sample.php
+++ environment-config-sample.php
@@ -1,6 +1,7 @@
 <?php
 
-//define( 'WP_SITEURL',         'http://development.example.org' );		// Use these if dev/staging environments have their own hostnames
+// Use these if dev/staging environments have their own hostnames
+//define( 'WP_SITEURL',         'http://development.example.org' );
 //define( 'WP_HOME',            'http://development.example.org' );
 
 define( 'DB_NAME',              'database_name_here' );
@@ -22,6 +23,7 @@ define( 'SECURE_AUTH_SALT',     'put your unique phrase here' );
 define( 'LOGGED_IN_SALT',       'put your unique phrase here' );
 define( 'NONCE_SALT',           'put your unique phrase here' );
 
+// Don't forget to disable display_errors and WP_DEBUG* on production
 ini_set( 'log_errors',          'On' );
 ini_set( 'display_errors',      'On' );
 ini_set( 'error_reporting',     E_ALL );
