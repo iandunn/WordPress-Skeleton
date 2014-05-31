40086f9bcbddd1c2a05495080ae11ea70ff8d511
Ian Dunn <ian@iandunn.name>
2014-05-31 13:28:52 -0700

    Remove deprecated FORCE_SSL_LOGIN constant.
    As of r28609-core it is no longer used for anything. FORCE_SSL_ADMIN should be used to encrypt the entire login/admin session.

--- wp-config.php
+++ wp-config.php
@@ -11,10 +11,8 @@ define( 'WP_CONTENT_DIR',       dirname( __FILE__ ) . '/content' );
 define( 'WP_CONTENT_URL',       'http://' . $_SERVER['HTTP_HOST'] . '/content' );
 define( 'WPLANG',               '' );
 
-// Uncomment the SSL constants if the site has a certificate
 define( 'DISALLOW_FILE_MODS',   true );
-//define( 'FORCE_SSL_LOGIN',	true );
-//define( 'FORCE_SSL_ADMIN',	true );
+define( 'FORCE_SSL_ADMIN',	true );
 
 // Multisite
 /*
